32c07d600792bbce8008f49a94f04c22df2042d5
Giang Nguyen <thuygiang152@gmail.com>
2019-10-31 22:37:42 +0200

    Flash Messaging

--- vidjot/app.js
+++ vidjot/app.js
@@ -3,6 +3,8 @@ const exphbs = require("express-handlebars");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
+const flash = require("connect-flash");
+const session = require("express-session");
 
 const app = express();
 
@@ -30,6 +32,25 @@ app.use(bodyParser.json());
 
 //Method Override Middleware
 app.use(methodOverride("_method"));
+
+//Express session middleware
+app.use(
+  session({
+    secret: "hyhythecat",
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+app.use(flash());
+
+//Global variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  next();
+});
 
 //Index Route
 app.get("/", (req, res) => {
@@ -100,6 +121,7 @@ app.put("/ideas/:id", (req, res) => {
 //Delete Idea
 app.delete("/ideas/:id", (req, res) => {
   Idea.remove({ _id: req.params.id }).then(() => {
+    req.flash("success_msg", "Video idea successfully deleted.");
     res.redirect("/ideas");
   });
 });
